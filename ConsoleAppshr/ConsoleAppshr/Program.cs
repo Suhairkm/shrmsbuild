23d619515a8e6a0cef4930be6f96394caa078482
suhairkm <suhairkmuhammed@gmail.com>
2022-04-01 12:13:15 +0530

    First commit

---
+++ ConsoleAppshr/ConsoleAppshr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppshr
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World! from suhair");
+        }
+    }
+}
